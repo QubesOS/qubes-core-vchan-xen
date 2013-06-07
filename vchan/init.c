333ca61841baa9da277373931c44416f49870a70
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-06-07 05:30:25 +0200

    init/client: properly get own domain ID
    Instead of hardcoded 0.
    This is required to properly support vchan client in VM.

--- vchan/init.c
+++ vchan/init.c
@@ -54,11 +54,13 @@ libvchan_t *libvchan_server_init(int domain, int port, size_t read_min, size_t w
 
 libvchan_t *libvchan_client_init(int domain, int port) {
     char xs_path[255];
+    char xs_path_watch[255];
     libvchan_t *ctrl;
     struct xs_handle *xs;
     char **vec;
     unsigned int count, len;
     char *dummy;
+    char *own_domid;
 
     if (!xc_handle) {
         xc_handle = xc_interface_open(NULL, NULL, 0);
@@ -68,21 +70,31 @@ libvchan_t *libvchan_client_init(int domain, int port) {
         }
     }
 
-    /* FIXME: get own domain ID instead of hardcoded "0" here */
-    snprintf(xs_path, sizeof(xs_path), "/local/domain/%d/data/vchan/0/%d", domain, port);
-
     /* wait for server to appear */
     xs = xs_open(0);
     if (!xs) {
         perror("xs_open");
         return NULL;
     }
-    xs_watch(xs, xs_path, xs_path);
+
+    own_domid = xs_read(xs, 0, "domid", &len);
+    if (!own_domid) {
+        fprintf(stderr, "Cannot get own domid\n");
+        xs_close(xs);
+        return NULL;
+    }
+
+    snprintf(xs_path, sizeof(xs_path), "/local/domain/%d/data/vchan/%s/%d",
+            domain, own_domid, port);
+    /* watch on this key as we might not have access to whole directory */
+    snprintf(xs_path_watch, sizeof(xs_path_watch), "%s/event-channel", xs_path);
+
+    xs_watch(xs, xs_path_watch, xs_path_watch);
     do {
         vec = xs_read_watch(xs, &count);
         if (vec)
             free(vec);
-        dummy = xs_read(xs, 0, xs_path, &len);
+        dummy = xs_read(xs, 0, xs_path_watch, &len);
     } while (!dummy);
     free(dummy);
     xs_close(xs);
