abce8838bdd59a5b1fbac101a6483f7a4a0fff81
Rafał Wojdyła <omeg@invisiblethingslab.com>
2015-06-01 21:16:32 +0200

    windos: removed default sign/wix scripts

--- windows/sign.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-IF NOT EXIST SIGN_CONFIG.BAT GOTO DONT_SIGN
-
-for /R %%f in (*.dll) do %SIGNTOOL% sign /v %CERT_CROSS_CERT_FLAG% /f %CERT_FILENAME% %CERT_PASSWORD_FLAG% /t http://timestamp.verisign.com/scripts/timestamp.dll %%f
-
-:DONT_SIGN
